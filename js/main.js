b040937c0b5ab1f6fc444cbe5b8d73fb9e33b88b
Yousef fathy <yk972268@gmail.com>
2025-02-05 16:35:49 +0200

    pig fixes and adde icons and title

--- js/main.js
+++ js/main.js
@@ -1,77 +1,24 @@
-let plus = document.querySelectorAll(".fa-plus");
-let minus = document.querySelectorAll(".fa-minus");
-let amountShow = document.querySelectorAll(".amount-show");
-let num1 = 0;
-let num2 = 0;
-let num3 = 0;
-let num4 = 0;
+document.addEventListener("DOMContentLoaded", function () {
 
-amountShow[0].innerHTML = 0;
-amountShow[1].innerHTML = 0;
-amountShow[2].innerHTML = 0;
-amountShow[3].innerHTML = 0;
+  document.querySelectorAll(".pro-card").forEach((card, index) => {
+    const plusBtn = card.querySelector(".fa-plus");
+    const minusBtn = card.querySelector(".fa-minus");
+    const amountDiv = card.querySelector(".amount");
 
-console.log(plus);
-console.log(minus);
-console.log(amountShow);
+    let amount = 0;
+    amountDiv.textContent = amount;
 
-function increase1() {
-  amountShow[0].innerHTML = num1 + 1;
-  num1 = Number(amountShow[0].innerHTML);
-}
+    plusBtn.addEventListener("click", function () {
+      amount++;
+      amountDiv.textContent = amount;
+    });
 
-function increase2() {
-  amountShow[1].innerHTML = num2 + 1;
-  num2 = Number(amountShow[1].innerHTML);
-}
-
-function increase3() {
-  amountShow[2].innerHTML = num3 + 1;
-  num3 = Number(amountShow[2].innerHTML);
-}
-
-function increase4() {
-  amountShow[3].innerHTML = num4 + 1;
-  num4 = Number(amountShow[3].innerHTML);
-}
-
-
-
-
-function decrease1() {
-
-  if (num1 === 0) {
-    amountShow[0].innerHTML = 0;
-  } else {
-    amountShow[0].innerHTML = num1 - 1;
-  }
-  num1 = Number(amountShow[0].innerHTML);
-}
-
-function decrease2() {
-  if (num2 === 0) {
-    amountShow[1].innerHTML = 0;
-  } else {
-    amountShow[1].innerHTML = num2 - 1;
-  }
-  num2 = Number(amountShow[1].innerHTML);
-}
-
-function decrease3() {
-  if (num3 === 0) {
-    amountShow[2].innerHTML = 0;
-  } else {
-    amountShow[2].innerHTML = num3 - 1;
-  }
-  num3 = Number(amountShow[2].innerHTML);
-}
-
-function decrease4() {
-  if (num4 === 0) {
-    amountShow[3].innerHTML = 0;
-  } else {
-    amountShow[3].innerHTML = num4 - 1;
-  }
-  num4 = Number(amountShow[3].innerHTML);
-}
+    minusBtn.addEventListener("click", function () {
+      if (amount > 0) {
+        amount--;
+        amountDiv.textContent = amount;
+      }
+    });
+  });
+});
 
